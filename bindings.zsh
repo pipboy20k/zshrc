12b5a36fd3f2d6eb8b1672b15200c7de4efdebaf
Sebastian Busse <pip@othello>
2025-06-19 07:45:45 +0200

    updated keybindings for ctrl left / right for backward / forward word

--- bindings.zsh
+++ bindings.zsh
@@ -5,9 +5,12 @@ bindkey "\e[5~" beginning-of-history
 bindkey "\e[6~" end-of-history
 bindkey "\e[3~" delete-char
 bindkey "\e[2~" quoted-insert
-bindkey "\e[5C" forward-word
+# bindkey "\e[5C" forward-word
+bindkey "^[[1;5C" forward-word
+# bindkey "^[[1;5C" forward-word
 bindkey "\eOc" emacs-forward-word
-bindkey "\e[5D" backward-word
+# bindkey "\e[5D" backward-word
+bindkey "^[[1;5D" backward-word
 bindkey "\eOd" emacs-backward-word
 bindkey "\ee[C" forward-word
 bindkey "\ee[D" backward-word
